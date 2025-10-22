5361cb479cf170801132e35f2d12080c42842f77
Semie947 <monyaharicheme@gmail.com>
2025-10-22 19:06:29 +0200

    Update and rename README.md to Semie veer.md
    Signed-off-by: Semie947 <monyaharicheme@gmail.com>

---
+++ Settings.js
@@ -0,0 +1,5 @@
+module.exports = {
+  SESSION_ID: " ",  // add your Session Id make sure it starts with semie ~
+  OWNER_NUMBER: "263788755586", // PUT OWNER NUMBER HERE
+  PREFIX: "." // prefix (e.g., ., /, !, *)
+};
